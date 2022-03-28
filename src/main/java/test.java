edb02088137b35b94313ada30ce0e6140afa919e
6351922238 <96417197+6351922238@users.noreply.github.com>
2022-03-28 22:32:04 +0800

    Update test.java

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("你好git");
+        System.out.println("你好1111");
     }
 }
